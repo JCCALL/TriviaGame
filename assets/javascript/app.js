4edb5af6666a2b33e0de3ff937f9b50ffc02ded6
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-03-24 14:31:59 -0600

    updated script

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var clock = 60;
+var clockRunning = false;
+var intervalId;
+var correctCount = 0;
+var missedCount = 0;
+var unansweredCount = 0;
+
+$(document).ready(function () {
+
+    $("#gameplay").hide();
+    $("#results").hide();
+
+
+    $("#stButton").on("click", start);
+    $("#done").on("click", finish); 
+    
+    function start() {
+        $("#start").hide();
+        $("#gameplay").show();
+
+        if(!clockRunning) {
+            intervalId = setInterval(count, 1000);
+            clockRunning = true;
+        };
+        return;
+    };
+
+    function finish() {
+        $("#results").show();
+        $("#gameplay").hide();
+        clearInterval(intervalId);
+        clockRunning = false;
+    };
+
+    function count() {
+        clock--;
+        $("#number").html(clock + " seconds");
+    }
+    
+});
