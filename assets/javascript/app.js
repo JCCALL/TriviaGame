ec46fff8568b927f4ded2bd3c5b33dd61331139b
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-03-25 21:00:28 -0600

    update game end code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,6 +32,7 @@ $(document).ready(function () {
         clearInterval(intervalId);
         clockRunning = false;
         clock = 60;
+        gameend()
     };
 
     function count() {
@@ -50,5 +51,25 @@ $(document).ready(function () {
         
     }
     
-    
+    function gameend() {
+        var Q1 = $('input:radio[name="q1"]:checked').val();
+        var Q2 = $('input:radio[name="q2"]:checked').val();
+        var Q3 = $('input:radio[name="q3"]:checked').val();
+        var Q4 = $('input:radio[name="q4"]:checked').val();
+        var Q5 = $('input:radio[name="q5"]:checked').val();
+        var Q6 = $('input:radio[name="q6"]:checked').val();
+        var Q7 = $('input:radio[name="q7"]:checked').val();
+        var Q8 = $('input:radio[name="q8"]:checked').val();
+        var Q9 = $('input:radio[name="q9"]:checked').val();
+        var Q10 = $('input:radio[name="q10"]:checked').val();
+
+        if (Q1 === undefined){
+            unansweredCount++;
+        } else if (Q1 == ""){
+            correctCount++;
+        }else{
+            missedCount++;
+        }
+    };
+
 });
